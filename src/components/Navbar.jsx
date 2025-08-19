10bebc7be74f201918f2f4ea986383cb429b7d9a
MukundMS <mukund.madhavsharma23140@gmail.com>
2025-08-19 22:17:57 +0530

    ok

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
         <h1 className="logo">My Portfolio</h1>
         <ul className="nav-links">
           <li><a href="/About">About</a></li>
-          <li><a href="#">Projects</a></li>
+          <li><a href="https://github.com/mukundmadhavsharma23140-sudo/MMShop.git">Projects</a></li>
           <li><a href="https://drive.google.com/file/d/1ZNo_lJkuux-cNghplQv6UlW4ulHIoRDQ/view?usp=sharing">Resume</a></li>
           <li><a href="https://www.linkedin.com/in/mukundms170804">Contact</a></li>
         </ul>
---
+++ src/components/Projects.jsx
@@ -0,0 +1,11 @@
+function Projects() {
+  return (
+    <section id="projects">
+      <h2>Projects</h2>
+      <p>My projects will go here 🚀</p>
+    </section>
+  )
+}
+
+export default Projects
+
